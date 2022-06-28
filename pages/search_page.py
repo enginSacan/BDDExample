9492759267239c4ef242d104ad7a82fce751b128
Mehmet Engin Sacan <46751860+enginSacan@users.noreply.github.com>
2022-06-28 18:16:51 +0200

    Update search_page.py

--- pages/search_page.py
+++ pages/search_page.py
@@ -9,7 +9,11 @@ from hamcrest import assert_that, equal_to
 
 
 class SearchPage(BasePage):
-
+    """
+        This class is created for the search page elements and actions for ui tests
+        Properties are defined for the elements in the page and each property waiting
+        element could be clickable or visible
+    """
     @property
     def search_bar(self):
         locator = (By.XPATH, Variables.search_bar_xpath)
@@ -36,6 +40,10 @@ class SearchPage(BasePage):
         return BaseElement(self.driver, locator[0], locator[1])
 
     def search_word(self, text):
+        """
+        This function is searching the text that user gives in the search bar.
+        :param text: this variable is for the text that user want to search in the search bar.
+        """
         try:
             self.accept_button.click()
         except NoSuchElementException:
@@ -45,8 +53,15 @@ class SearchPage(BasePage):
         assert_that(self.driver.find_elements_by_xpath(Variables.search_result_xpath)[0].text, equal_to("https://teknasyon.com"))
 
     def open_search_page(self, url):
+        """
+        This function opens the search page in the browser.
+        :param url: URL of the search page
+        """
         self.driver.get(url)
         assert_that(self.google_icon.attribute("alt"), equal_to("Google"))
 
     def go_to(self):
+        """
+        This function clicks the search result element
+        """
         self.driver.find_elements_by_xpath(Variables.search_result_xpath)[0].click()
