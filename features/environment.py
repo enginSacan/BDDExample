dc138fceccf96e6225ea959ad19522ba50593500
Mehmet Engin Sacan <46751860+enginSacan@users.noreply.github.com>
2022-06-28 18:14:08 +0200

    Update environment.py

--- features/environment.py
+++ features/environment.py
@@ -1,10 +1,18 @@
 from selenium import webdriver
 from app.Application import Application
 
+__author__ = "Mehmet Engin Sacan"
+
+
+"""
+This file contains before and after actions for each scenario in the feature file.
+"""
+
 
 def before_scenario(context, scenario):
     context.driver = webdriver.Chrome()
     context.app = Application(context.driver)
+
 
 def after_scenario(context, scenario):
     context.driver.quit()
