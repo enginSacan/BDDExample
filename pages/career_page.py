37b7bfe589508afc157b1c234987217be117552d
Mehmet Engin Sacan <46751860+enginSacan@users.noreply.github.com>
2022-06-28 18:15:32 +0200

    Update career_page.py

--- pages/career_page.py
+++ pages/career_page.py
@@ -7,9 +7,18 @@ from pages.base_element import BaseElement
 from pages.base_page import BasePage
 from pages.variables import Variables
 
+__author__ = "Mehmet Engin Sacan"
+
 
 class CareerPage(BasePage):
+    """
+        This class is created for the career page elements and actions for ui tests
+        Properties are defined for the elements in the page and each property waiting
+        element could be clickable or visible.
 
+        Methods ,which start with check, are the assertion functions
+
+    """
     @property
     def description(self):
         locator = (By.XPATH, Variables.description_xpath)
@@ -29,6 +38,10 @@ class CareerPage(BasePage):
         return "Kodluyoruz" in self.description.text
 
     def choose_job(self, job):
+        """
+         This function is choosing the job that is given as parameter.
+        :param job: This variable is given by user for job title search
+        """
         jobs = self.driver.find_elements_by_xpath(Variables.jobs_xpath)
         time.sleep(1)
         for job_found in jobs:
