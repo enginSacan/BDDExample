ed079601a3e6384a082e0f8c8265097cbc6fb4aa
Mehmet Engin Sacan <46751860+enginSacan@users.noreply.github.com>
2022-06-28 18:12:12 +0200

    Update Application.py

--- app/Application.py
+++ app/Application.py
@@ -3,10 +3,16 @@ from pages.company_page import CompanyPage
 from pages.career_page import CareerPage
 from pages.job_page import JobPage
 
+__author__ = "Mehmet Engin Sacan"
+
 
 class Application:
+    """
+    This class constructing the pages for the application
+    """
     def __init__(self, driver):
         self.search_page = SearchPage(driver)
         self.company_page = CompanyPage(driver)
         self.career_page = CareerPage(driver)
         self.job_page = JobPage(driver)
+
