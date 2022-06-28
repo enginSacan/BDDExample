678beae98aa612f58b5832b2df7f4fa10f05399b
Mehmet Engin Sacan <46751860+enginSacan@users.noreply.github.com>
2022-06-28 18:15:10 +0200

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,10 @@
+__author__ = "Mehmet Engin Sacan"
+
+
 class BasePage:
+
+    """
+        This page class is the initialization of the webdriver
+    """
     def __init__(self, driver):
         self.driver = driver
